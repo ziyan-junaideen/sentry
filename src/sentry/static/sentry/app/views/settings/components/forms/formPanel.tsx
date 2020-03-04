2a71a56ba529bf59da53b2cf80230cc6c4c64e8d
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-03-03 17:03:32 -0800

    ref(ui): moved security and privacy section

--- src/sentry/static/sentry/app/views/settings/components/forms/formPanel.tsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/formPanel.tsx
@@ -20,7 +20,7 @@ type Props = {
 
   // TODO(ts): See if this is still in use
   access?: Scope[];
-  features?: string[];
+  features?: {[key: string]: any};
 
   additionalFieldProps: {[key: string]: any};
 
---
+++ src/sentry/static/sentry/app/views/settings/organizationSecurityAndPrivacy/organizationSecurityAndPrivacy.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Feature from 'app/components/acl/feature';
+import FeatureDisabled from 'app/components/acl/featureDisabled';
+import {PanelAlert} from 'app/components/panels';
+import {t} from 'app/locale';
+import withOrganization from 'app/utils/withOrganization';
+
+import OrganizationSecurityAndPrivacyContent, {
+  OrganizationSecurityAndPrivacyContentProps,
+} from './organizationSecurityAndPrivacyContent';
+
+const OrganizationSecurityAndPrivacy = ({
+  organization,
+  ...props
+}: OrganizationSecurityAndPrivacyContentProps) => (
+  <Feature
+    features={['datascrubbers-v2']}
+    organization={organization}
+    renderDisabled={() => (
+      <FeatureDisabled
+        alert={PanelAlert}
+        features={organization.features}
+        featureName={t('Security & Privacy - new')}
+      />
+    )}
+  >
+    <OrganizationSecurityAndPrivacyContent {...props} organization={organization} />
+  </Feature>
+);
+
+export default withOrganization(OrganizationSecurityAndPrivacy);
